478821de30b876c9959adab482b5d68ef7164460
shelling <navyblueshellingford@gmail.com>
2010-12-18 13:50:46 +0800

    initial commit

---
+++ lib/UNIVERSAL/filename.pm
@@ -0,0 +1,32 @@
+package UNIVERSAL::filename;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+UNIVERSAL::filename -
+
+=head1 SYNOPSIS
+
+  use UNIVERSAL::filename;
+
+=head1 DESCRIPTION
+
+UNIVERSAL::filename is
+
+=head1 AUTHOR
+
+shelling E<lt>navyblueshellingford@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
